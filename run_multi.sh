bed00222fa915c558c0baa79c99b243a55a170e5
shreymittal1000 <shreymittal1000@gmail.com>
2025-06-17 13:33:41 +0200

    changing base

--- run_multi.sh
+++ run_multi.sh
@@ -15,25 +15,25 @@ if [ "$game" = "base" ]; then
     for perm in {0..4}
     do
       echo "Run #$i: $model_0 vs $model_1 -> $game with SPV: $spv and GPV: v3.$perm"
-      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v3.$perm"
+      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v6.$perm"
       
       echo "Run #$i: $model_0 vs $model_1 -> $game with SPV: $spv and GPV: v4.$perm"
-      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v4.$perm"
+      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v7.$perm"
       
       echo "Run #$i: $model_0 vs $model_1 -> $game with SPV: $spv and GPV: v5.$perm"
-      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v5.$perm"
+      python3 main.py "$model_0" "$model_1" "$game" "$spv" "v8.$perm"
     done
   done
 else
   for i in {1..10}
   do
     echo "Run #$i: $model_0 vs $model_1 -> Game: $game with SPV: $spv and GPV: v3"
-    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v3"
+    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v6"
     
     echo "Run #$i: $model_0 vs $model_1 -> Game: $game with SPV: $spv and GPV: v4"
-    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v4"
+    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v7"
     
     echo "Run #$i: $model_0 vs $model_1 -> Game: $game with SPV: $spv and GPV: v5"
-    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v5"
+    python3 main.py "$model_0" "$model_1" "$game" "$spv" "v8"
   done
 fi
